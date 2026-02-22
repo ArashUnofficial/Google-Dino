aac5c03ea1368e57085ccead7e752c225003c787
Arash <arash.assari86@gmail.com>
2026-02-22 15:05:01 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Google_Dino_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            MainTimer = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            label2 = new Label();
+            DinoPic = new PictureBox();
+            Scoretxt = new Label();
+            GameOvertxt = new Label();
+            Restarttxt = new Label();
+            JumpTimer = new System.Windows.Forms.Timer(components);
+            CactusPic = new PictureBox();
+            BirdPic = new PictureBox();
+            ScoreNumtxt = new Label();
+            TopScoretxt = new Label();
+            TopScoreNumtxt = new Label();
+            DecorativeDinoPic = new PictureBox();
+            Hinttxt = new Label();
+            ((System.ComponentModel.ISupportInitialize)DinoPic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)CactusPic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BirdPic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DecorativeDinoPic).BeginInit();
+            SuspendLayout();
+            // 
+            // MainTimer
+            // 
+            MainTimer.Enabled = true;
+            MainTimer.Interval = 1;
+            MainTimer.Tick += MainTimer_Tick;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.DimGray;
+            label1.Location = new Point(-7, 367);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(987, 8);
+            label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Black;
+            label2.Location = new Point(-7, 373);
+            label2.Margin = new Padding(0);
+            label2.Name = "label2";
+            label2.Size = new Size(987, 79);
+            label2.TabIndex = 0;
+            // 
+            // DinoPic
+            // 
+            DinoPic.Image = Properties.Resources.normal_running;
+            DinoPic.Location = new Point(183, 317);
+            DinoPic.Margin = new Padding(0);
+            DinoPic.Name = "DinoPic";
+            DinoPic.Size = new Size(50, 50);
+            DinoPic.SizeMode = PictureBoxSizeMode.AutoSize;
+            DinoPic.TabIndex = 1;
+            DinoPic.TabStop = false;
+            // 
+            // Scoretxt
+            // 
+            Scoretxt.AutoSize = true;
+            Scoretxt.BackColor = SystemColors.Control;
+            Scoretxt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Scoretxt.Location = new Point(9, 9);
+            Scoretxt.Margin = new Padding(0);
+            Scoretxt.Name = "Scoretxt";
+            Scoretxt.Size = new Size(70, 28);
+            Scoretxt.TabIndex = 2;
+            Scoretxt.Text = "Score :";
+            // 
+            // GameOvertxt
+            // 
+            GameOvertxt.AutoSize = true;
+            GameOvertxt.BackColor = SystemColors.Control;
+            GameOvertxt.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            GameOvertxt.Location = new Point(428, 103);
+            GameOvertxt.Margin = new Padding(0);
+            GameOvertxt.Name = "GameOvertxt";
+            GameOvertxt.Size = new Size(117, 28);
+            GameOvertxt.TabIndex = 2;
+            GameOvertxt.Tag = "";
+            GameOvertxt.Text = "Game Over";
+            GameOvertxt.Visible = false;
+            // 
+            // Restarttxt
+            // 
+            Restarttxt.AutoSize = true;
+            Restarttxt.BackColor = SystemColors.Control;
+            Restarttxt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Restarttxt.Location = new Point(375, 162);
+            Restarttxt.Margin = new Padding(0);
+            Restarttxt.Name = "Restarttxt";
+            Restarttxt.Size = new Size(222, 28);
+            Restarttxt.TabIndex = 2;
+            Restarttxt.Tag = "";
+            Restarttxt.Text = "Press Any Key To Restart";
+            Restarttxt.Visible = false;
+            // 
+            // JumpTimer
+            // 
+            JumpTimer.Interval = 1;
+            JumpTimer.Tick += JumpTimer_Tick;
+            // 
+            // CactusPic
+            // 
+            CactusPic.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            CactusPic.BackgroundImageLayout = ImageLayout.None;
+            CactusPic.Image = Properties.Resources._6;
+            CactusPic.Location = new Point(2000, 334);
+            CactusPic.Margin = new Padding(0);
+            CactusPic.Name = "CactusPic";
+            CactusPic.Size = new Size(32, 33);
+            CactusPic.SizeMode = PictureBoxSizeMode.AutoSize;
+            CactusPic.TabIndex = 1;
+            CactusPic.TabStop = false;
+            // 
+            // BirdPic
+            // 
+            BirdPic.Image = Properties.Resources.bird;
+            BirdPic.Location = new Point(5500, 227);
+            BirdPic.Margin = new Padding(0);
+            BirdPic.Name = "BirdPic";
+            BirdPic.Size = new Size(50, 41);
+            BirdPic.SizeMode = PictureBoxSizeMode.StretchImage;
+            BirdPic.TabIndex = 1;
+            BirdPic.TabStop = false;
+            // 
+            // ScoreNumtxt
+            // 
+            ScoreNumtxt.AutoSize = true;
+            ScoreNumtxt.BackColor = SystemColors.Control;
+            ScoreNumtxt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ScoreNumtxt.Location = new Point(79, 9);
+            ScoreNumtxt.Margin = new Padding(0);
+            ScoreNumtxt.Name = "ScoreNumtxt";
+            ScoreNumtxt.Size = new Size(23, 28);
+            ScoreNumtxt.TabIndex = 2;
+            ScoreNumtxt.Text = "0";
+            // 
+            // TopScoretxt
+            // 
+            TopScoretxt.AutoSize = true;
+            TopScoretxt.BackColor = SystemColors.Control;
+            TopScoretxt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            TopScoretxt.Location = new Point(9, 42);
+            TopScoretxt.Margin = new Padding(0);
+            TopScoretxt.Name = "TopScoretxt";
+            TopScoretxt.Size = new Size(107, 28);
+            TopScoretxt.TabIndex = 2;
+            TopScoretxt.Text = "Top Score :";
+            // 
+            // TopScoreNumtxt
+            // 
+            TopScoreNumtxt.AutoSize = true;
+            TopScoreNumtxt.BackColor = SystemColors.Control;
+            TopScoreNumtxt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            TopScoreNumtxt.Location = new Point(116, 42);
+            TopScoreNumtxt.Margin = new Padding(0);
+            TopScoreNumtxt.Name = "TopScoreNumtxt";
+            TopScoreNumtxt.Size = new Size(23, 28);
+            TopScoreNumtxt.TabIndex = 2;
+            TopScoreNumtxt.Text = "0";
+            // 
+            // DecorativeDinoPic
+            // 
+            DecorativeDinoPic.Image = Properties.Resources.crouched_running;
+            DecorativeDinoPic.Location = new Point(378, 138);
+            DecorativeDinoPic.Margin = new Padding(0);
+            DecorativeDinoPic.Name = "DecorativeDinoPic";
+            DecorativeDinoPic.Size = new Size(219, 103);
+            DecorativeDinoPic.SizeMode = PictureBoxSizeMode.StretchImage;
+            DecorativeDinoPic.TabIndex = 1;
+            DecorativeDinoPic.TabStop = false;
+            DecorativeDinoPic.Tag = "pause";
+            DecorativeDinoPic.Visible = false;
+            // 
+            // Hinttxt
+            // 
+            Hinttxt.AutoSize = true;
+            Hinttxt.BackColor = SystemColors.Control;
+            Hinttxt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Hinttxt.Location = new Point(225, 285);
+            Hinttxt.Margin = new Padding(0);
+            Hinttxt.Name = "Hinttxt";
+            Hinttxt.Size = new Size(522, 28);
+            Hinttxt.TabIndex = 2;
+            Hinttxt.Tag = "pause";
+            Hinttxt.Text = "Press R For Restart, Esc to Resume The Game or E For Exit !!";
+            Hinttxt.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(973, 450);
+            Controls.Add(Hinttxt);
+            Controls.Add(Restarttxt);
+            Controls.Add(GameOvertxt);
+            Controls.Add(TopScoreNumtxt);
+            Controls.Add(ScoreNumtxt);
+            Controls.Add(TopScoretxt);
+            Controls.Add(Scoretxt);
+            Controls.Add(BirdPic);
+            Controls.Add(CactusPic);
+            Controls.Add(DecorativeDinoPic);
+            Controls.Add(DinoPic);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            KeyPreview = true;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Google Challenging Game";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)DinoPic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CactusPic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BirdPic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DecorativeDinoPic).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private PictureBox DinoPic;
+        private Label Scoretxt;
+        private Label GameOvertxt;
+        private Label Restarttxt;
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Timer JumpTimer;
+        private PictureBox CactusPic;
+        private PictureBox BirdPic;
+        private Label ScoreNumtxt;
+        private Label TopScoretxt;
+        private Label TopScoreNumtxt;
+        private PictureBox DecorativeDinoPic;
+        private Label Hinttxt;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System.Security.Cryptography;
+
+namespace Google_Dino_Final
+{
+    public partial class Form1 : Form
+    {
+
+        bool JumpMore = true;
+        bool GameOver = false;
+        bool Crouched = false;
+        bool GamePaused = false;
+        bool WasJumping = false;
+        byte a;
+        int ObjectMoveSpeed = 5;
+        int ChosenObj;
+        Random NumPicker = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GameRestart()  // شروع دوباره  *****
+        {
+            // برگرداندن به حالت پیش فرض
+            GameOver = false;
+            MainTimer.Start();
+            JumpMore = true;
+            GameOvertxt.Visible = false;
+            Restarttxt.Visible = false;
+            ObjectMoveSpeed = 5;
+            DinoPic.ImageLocation = @"img\normal-running.gif";
+            DinoPic.Top = 317;
+            BirdPic.Left = 8500;
+            CactusPic.Left = 1500;
+            ScoreNumtxt.Text = "0";
+        }
+
+        private void JumpTimer_Tick(object sender, EventArgs e)  // پریدن به کمک تایمر  ******
+        {
+            if (JumpMore) // بالا رفتن تا جای ممکن
+            {
+                DinoPic.Top -= 9;
+                JumpTimer.Interval += 1;
+                if (DinoPic.Top == 137) JumpMore = false;
+            }
+            else // پایین آمدن
+            {
+                DinoPic.Top += 9;
+                JumpTimer.Interval -= 1;
+                if (DinoPic.Top == 317)
+                {
+                    JumpTimer.Stop();
+                    JumpMore = true;
+                }
+            }
+        }
+        
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+
+            if (BirdPic.Left == CactusPic.Left)
+                BirdPic.Left += 500;
+
+            if ((CactusPic.Left >= this.Width - 5) && (CactusPic.Left <= this.Width + 5))   // انتخاب کاکتوس تصادفی
+            {
+                ChosenObj = NumPicker.Next(1, 8);
+                CactusPic.ImageLocation = "img\\" + ChosenObj + ".png";
+            }
+            CactusPic.Top = label1.Top - CactusPic.Height;
+
+            if (BirdPic.Left >= this.Width)  // انتخاب مکان پرنده
+            {
+                switch (NumPicker.Next(1, 4))
+                {
+                    case 1:
+                        {
+                            BirdPic.Top = 324;
+                            break;
+                        }
+                    case 2:
+                        {
+                            BirdPic.Top = 287;
+                            break;
+                        }
+                    case 3:
+                        {
+                            BirdPic.Top = 227;
+                            break;
+                        }
+                }
+            }
+
+            BirdPic.Left -= ObjectMoveSpeed;    // حرکت دادن پرنده
+            CactusPic.Left -= ObjectMoveSpeed;  // حرکن کاکتوس
+
+            // برخورد با اجسام   *******
+            if (DinoPic.Bounds.IntersectsWith(CactusPic.Bounds) || DinoPic.Bounds.IntersectsWith(BirdPic.Bounds))
+            {
+                DinoPic.ImageLocation = @"img\dead.png";
+                MainTimer.Stop();
+                JumpTimer.Stop();
+                GameOver = true;
+                if ((int.Parse(TopScoreNumtxt.Text)) < (int.Parse(ScoreNumtxt.Text)))
+                {
+                    TopScoreNumtxt.Text = ScoreNumtxt.Text;
+                }
+                Restarttxt.Visible = true;
+                GameOvertxt.Visible = true;
+            }
+
+            if (CactusPic.Right <= 0)   // برگشتن کاکتوس به نقطه اول
+            {
+                ScoreNumtxt.Text = (int.Parse(ScoreNumtxt.Text) + 1).ToString();
+                CactusPic.Left = 1000;
+                a++;
+            }
+
+            if (BirdPic.Right <= 0)
+            {
+                BirdPic.Left += 5000;
+                ScoreNumtxt.Text = (int.Parse(ScoreNumtxt.Text) + 1).ToString();
+                a++;
+            }
+
+            if (a > 7)
+            {
+                ObjectMoveSpeed++;
+                a = 0;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)   // فشردن کلید ها  ******
+        {
+            if (!GameOver)
+            {
+                if (("Up" == e.KeyCode.ToString()) && (317 == DinoPic.Top) && (!Crouched) && (!GamePaused))  // جهت بالا و پریدن
+                {
+                    JumpTimer.Start();
+                }
+                else if (("Down" == e.KeyCode.ToString()) && (317 == DinoPic.Top) && (!GamePaused))  // جهت پایین و خم شدن
+                {
+                    Crouched = true;
+                    DinoPic.Top = 330;
+                    DinoPic.ImageLocation = @"img\crouched-running.gif";
+                }
+                else if ("Escape" == e.KeyCode.ToString())  //  منوی پاز  ***********
+                {
+
+                    MainTimer.Enabled = !MainTimer.Enabled;
+                    if (JumpTimer.Enabled)
+                    {
+                        WasJumping = true;
+                        JumpTimer.Stop();
+                    }
+                    else if (WasJumping)
+                    {
+                        WasJumping = false;
+                        JumpTimer.Start();
+                    }
+
+                    GamePaused = !GamePaused;
+
+                    Hinttxt.Visible = !Hinttxt.Visible;
+                    DecorativeDinoPic.Visible = !DecorativeDinoPic.Visible;
+                    ScoreNumtxt.Visible = !ScoreNumtxt.Visible;
+                    Scoretxt.Visible = !Scoretxt.Visible;
+                    TopScoreNumtxt.Visible = !TopScoreNumtxt.Visible;
+                    TopScoretxt.Visible = !TopScoretxt.Visible;
+                    label1.Visible = !label1.Visible;
+                    label2.Visible = !label2.Visible;
+                    CactusPic.Visible = !CactusPic.Visible;
+                    DinoPic.Visible = !DinoPic.Visible;
+                    BirdPic.Visible = !BirdPic.Visible;
+                }
+                else if ((GamePaused) && ("R" == e.KeyCode.ToString()))  // فراخانی تابع برای شروع دوباره
+                    GameRestart();
+                else if ((GamePaused) && ("E" == e.KeyCode.ToString()))  // بستن بازی
+                    this.Close();
+            }
+            else
+                GameRestart();
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)  // بالا آمدن کلید  ******
+        {
+            if ("Down" == e.KeyCode.ToString() && (Crouched))
+            {
+                Crouched = false;
+                DinoPic.Top = 317;
+                DinoPic.ImageLocation = @"img\normal-running.gif";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
